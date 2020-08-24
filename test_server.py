2f14beb4c0de67235bcde772556eb4168f9c76e7
Chess <koy321@naver.com>
2020-08-24 18:13:15 +0900

    about 페이지 추가됨

--- test_server.py
+++ test_server.py
@@ -1,10 +1,17 @@
 import flask
+import server_logger
 
 app = flask.Flask(__name__)
 
-@@app.route("/")
+@app.route("/")
 def hello_world():
 	return "Hello world!"
+    
+@app.route("/about")
+def hello_world():
+	return "the about"
 	
 if __name__ == '__main__':
-	app.run()
+    host_ = server_logger.get_host()
+    port_ = server_logger.get_port()
+	app.run(host = host_, port = port_)
