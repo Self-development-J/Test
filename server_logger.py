161013be7eb2846e0242535af77e1cb1d85ffa33
Chess <koy321@naver.com>
2020-08-24 18:03:12 +0900

    module for get server module

---
+++ server_logger.py
@@ -0,0 +1,18 @@
+#
+
+def def_log():
+	pass
+    
+def get_host():
+    host = "0.0.0.0"
+    return localhost
+	
+def get_port():
+    port = "8080"
+    return port
+    
+def main():
+	pass
+	
+if __name__ == '__main__':
+	main()
