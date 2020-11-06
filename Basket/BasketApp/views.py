27842b2f0f5786838860e554bc523aa970ae64a7
jonjae <koy321@naver.com>
2020-11-07 00:26:24 +0900

    2020.11.07 start

--- Basket/BasketApp/views.py
+++ Basket/BasketApp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
+
+from django.http import HttpResponse
 # from boardapp.models import Boards
 
 # Create your views here.
 
+def index(request):
+    return HttpResponse("안녕 씨발련아. 너 진짜 좆같이 생겼구나?")
